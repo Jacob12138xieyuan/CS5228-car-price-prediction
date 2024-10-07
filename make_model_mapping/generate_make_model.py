1eaa0f98bd47518629c61fad53f0c1f165b95a31
jacob <xiey0017@gmail.com>
2024-10-07 18:03:28 +0800

    added make_model mapping

--- make_model_mapping/generate_make_model.py
+++ make_model_mapping/generate_make_model.py
@@ -1,18 +1,20 @@
 import pandas as pd
 
-# drop_duplicated_column = "baseModel"
-drop_duplicated_column = "Model"
-
 # Read the CSV file and select the "Make" and "baseModel" columns
 df = pd.read_csv("all-vehicles-model.csv", sep=";")
-df = df[["Make", drop_duplicated_column]]
+df = df[["Make", "baseModel", "Model"]]
+
+# convert to lower case
+df["Make"] = df["Make"].str.lower()
+df["baseModel"] = df["baseModel"].str.lower()
+df["Model"] = df["Model"].str.lower()
 
 # Remove duplicates based on "Make" and "baseModel", sort the DataFrame, and reset the index
-df.drop_duplicates(["Make", drop_duplicated_column], inplace=True)
-df.sort_values(["Make", drop_duplicated_column], inplace=True)
+df.drop_duplicates(["Make", "baseModel", "Model"], inplace=True)
+df.sort_values(["Make", "baseModel", "Model"], inplace=True)
 df.reset_index(drop=True, inplace=True)
 
-print(df[df["Make"] == "Land Rover"])
+print(df[df["Make"] == "land rover"])
 
 # Save the processed DataFrame to a new CSV file without the index column
 df.to_csv("make_model.csv", index=False)

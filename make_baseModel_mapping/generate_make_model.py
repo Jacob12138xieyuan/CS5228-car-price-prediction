52442b41155852d48d5c41b5adaf522574d2264f
jacob <xiey0017@gmail.com>
2024-10-07 16:24:50 +0800

    added make_baseModel mapping

--- make_baseModel_mapping/generate_make_model.py
+++ make_baseModel_mapping/generate_make_model.py
@@ -1,9 +1,10 @@
 import pandas as pd
 
-df = pd.read_csv("all-vehicles-model.csv", sep=";")
+df = pd.read_csv("make_baseModel_mapping/all-vehicles-model.csv", sep=";")
 df = df[["Make", "baseModel"]]
 df.drop_duplicates(["Make", "baseModel"], inplace=True)
 df.sort_values(["Make", "baseModel"], inplace=True)
+df.reset_index(drop=True, inplace=True)
 
 print(df[df["Make"] == "Land Rover"])
-df.to_csv("make_baseModel.csv")
+df.to_csv("make_baseModel_mapping/make_baseModel.csv")
